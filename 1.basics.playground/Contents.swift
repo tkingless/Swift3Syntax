30128dc3dff04e631a56efd1b7be13cf02a3f602
tkingless <t.kingless@yahoo.com>
2017-01-14 14:39:05 +0800

    delete xcworkspace

--- 1.basics.playground/Contents.swift
+++ 1.basics.playground/Contents.swift
@@ -10,3 +10,22 @@ var currentLoginAttempt = 0
 var x = 0.0, y = 0.0, z = 0.0
 
 //to indicate that the variable can store String values
+var welcomeMessage: String
+
+//The welcomeMessage variable can now be set to any string value without error
+welcomeMessage = "Hello"
+
+var red, green, blue: Double
+
+let π = 3.14159
+let 你好 = "你好世界"
+let 🐶🐮 = "dogcow" //crtl + cmd + space to insert icon
+
+var friendlyWelcome = "Hello!"
+friendlyWelcome = "Bonjour!"
+
+print(friendlyWelcome)
+
+print("The current value of friendlyWelcome is \(friendlyWelcome)")
+// Prints "The current value of friendlyWelcome is Bonjour!"
+
