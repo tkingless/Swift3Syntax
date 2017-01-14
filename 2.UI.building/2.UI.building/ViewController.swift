e2b4fcda8bd23fac57c1a567fe85de1e15a32057
tkingless <t.kingless@yahoo.com>
2017-01-14 15:05:54 +0800

    messy change

---
+++ 2.UI.building/2.UI.building/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  2.UI.building
+//
+//  Created by Tsang King Kwun on 14/1/2017.
+//  Copyright © 2017 Swift3. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
