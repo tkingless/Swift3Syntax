cee8f1f3f0c89fdcdb1aa8439c107683e4c47ca9
tkingless <t.kingless@yahoo.com>
2017-01-14 14:21:40 +0800

    init proj

---
+++ Swift3Tutorial.playground/Contents.swift
@@ -0,0 +1,8 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//Constants and variables must be declared before they are used. You declare constants with the let keyword and variables with the var keyword.
+
+let maximumNumberOfLoginAttempts = 10
+var currentLoginAttempt = 0
