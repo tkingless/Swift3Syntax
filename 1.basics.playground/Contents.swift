6b9e3abd364064d216ad296b41de991300ec2001
tkingless <t.kingless@yahoo.com>
2017-01-14 14:25:42 +0800

    renaming

--- 1.basics.playground/Contents.swift
+++ 1.basics.playground/Contents.swift
@@ -1,8 +1,12 @@
-//: Playground - noun: a place where people can play
-
 import UIKit
 
 //Constants and variables must be declared before they are used. You declare constants with the let keyword and variables with the var keyword.
 
 let maximumNumberOfLoginAttempts = 10
 var currentLoginAttempt = 0
+
+//You can declare multiple constants or multiple variables on a single line, separated by commas
+
+var x = 0.0, y = 0.0, z = 0.0
+
+//to indicate that the variable can store String values
