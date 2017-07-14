a4c4376ed349e1cca06f54a4a1beae201b385cfb
tkingless <t.kingless@yahoo.com>
2017-07-14 09:09:19 +0800

    Initial Commit

---
+++ UIplayground/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UIplayground
+//
+//  Created by Tsang King Kwun on 14/7/2017.
+//  Copyright © 2017 Swift3. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
